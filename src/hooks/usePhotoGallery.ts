9745b6da5bf9bbebe4c68c46ed0c88ef05adebd7
alvaro santos <alvarossmiguel@gmail.com>
2021-05-13 21:20:18 -0300

    saving photos to storage (web)

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -11,10 +11,30 @@ import {
   FilesystemDirectory,
 } from "@capacitor/core";
 
-export function usePhotoGallery() {
-  const [photos, setPhotos] = useState<Photo[]>([]);
+const PHOTO_STORAGE = "photos";
 
+export function usePhotoGallery() {
+  
+  const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+  const [photos, setPhotos] = useState<Photo[]>([]);
   const { getPhoto } = useCamera();
+  const { get, set } = useStorage();
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const photosString = await get(PHOTO_STORAGE);
+      const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+      for (let photo of photos) {
+        const file = await readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photos);
+    };
+    loadSaved();
+  }, [get, readFile]);
 
   const takePhoto = async () => {
     const cameraPhoto = await getPhoto({
@@ -23,14 +43,27 @@ export function usePhotoGallery() {
       quality: 100,
     });
     const fileName = new Date().getTime() + ".jpeg";
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: cameraPhoto.webPath,
-      },
-      ...photos,
-    ];
+    const savedFileImage = await savePicture(cameraPhoto, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+    set(PHOTO_STORAGE, JSON.stringify(newPhotos));
+  };
+
+  const savePicture = async (
+    photo: CameraPhoto,
+    fileName: string
+  ): Promise<Photo> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
   };
 
   return {
